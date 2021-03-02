eb8b14deaea8e2302d0a81ece9a0dda69a3117fc
sudo-ToR <55355210+sudo-ToR@users.noreply.github.com>
2021-03-02 12:04:21 +0100

    Delete mysql_users.sql

--- mysql/mysql_users.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE wordpress;
-CREATE USER 'usertest'@'localhost' IDENTIFIED BY 'password';
-GRANT SHOW DATABASES ON * . * TO 'usertest'@'localhost';
-CREATE USER 'usertestbis'@'localhost' IDENTIFIED BY 'password';
-GRANT SHOW DATABASES ON * . * TO 'usertestbis'@'localhost';
-CREATE USER 'root'@'%' IDENTIFIED BY 'root';
-GRANT ALL PRIVILEGES ON * . * TO 'root'@'%';
-FLUSH PRIVILEGES;
