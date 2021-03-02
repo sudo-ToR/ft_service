51990e8edf7528bec10dbf033209d8c998880d2f
user42 <lina.noirot@gmail.com>
2021-03-02 18:11:09 +0100

    fixing nginx

--- srcs/nginx/install_nginx.sh
+++ srcs/nginx/install_nginx.sh
@@ -5,8 +5,3 @@ mkdir /www/phpmyadmin/
 chown -R www:www /var/lib/nginx
 chown -R www:www /www
 mkdir /run/nginx
-openrc boot
-rc-update add nginx boot
-# rc-update add php-fpm7 default
-# rc-service php-fpm7 start
-rc-service -S nginx start
