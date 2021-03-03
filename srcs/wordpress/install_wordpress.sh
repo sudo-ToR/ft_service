f351c7c7f194395bbc455d9afcd2459beccb22ea
user42 <lina.noirot@gmail.com>
2021-03-03 17:52:18 +0100

    wp OKKKKKKK

--- srcs/wordpress/install_wordpress.sh
+++ srcs/wordpress/install_wordpress.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 chmod -R  777 /www/
-if ! $(su - wp -c 'wp core is-installed'); then
+su - wp -c 'wp core is-installed --path=/www/wp'
+if [ $? == 1 ]; then
 su - wp -c 'wp core install --url=172.17.0.2:5050 --title=Example --admin_user=admin --admin_password=password --admin_email=info@example.com --skip-email --path=/www/wp/'
 su - wp -c 'wp user create test1 test1@example.com --role=author --path=/www/wp' 
 su - wp -c 'wp user create test2 test2@example.com --role=editor --path=/www/wp' 
